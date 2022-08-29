645750e1b488abcd00f0715c10d3dbd8727be07d
Matt <matt@randomforestweb.com>
2022-08-29 22:40:33 +0900

    first commit

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+    //DB Params
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASSWORD', 'YOUR_PASSWORD');
+    define('DB_NAME', 'YOUR_DBNAME');
+
+    //App Root
+    define('APPROOT', dirname(dirname(__FILE__)));
+    
+    //URL Root
+    define('URLROOT', 'http://localhost/YOUR_EXTENSION');
+    
+    //Site Name
+    define('SITENAME', 'YOUR_SITENAME');
